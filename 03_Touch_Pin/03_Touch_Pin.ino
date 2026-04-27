2ba37af0b050ada91ab3d97b7a3e2b4d27f84a55
ooikk <105415945+ooikk@users.noreply.github.com>
2026-04-27 11:54:21 +0800

    Add threshold detection

--- 03_Touch_Pin/03_Touch_Pin.ino
+++ 03_Touch_Pin/03_Touch_Pin.ino
@@ -1,20 +1,25 @@
 // Choose a Touch-enabled pin from your diagram
 // GPIO 1 is labeled as TOUCH1
-const int touchPin = 4; 
+const int touchPin = 4;
 int touchValue = 0;
+#define TOUCHTHRESHOLD 50000
 
 void setup() {
   Serial.begin(115200);
-  delay(1000); // Give the Serial Monitor time to connect
+  delay(1000);  // Give the Serial Monitor time to connect
   Serial.println("ESP32-S3 Touch Test Initialized");
 }
 
 void loop() {
   // Read the touch value (0 to 100 on S3)
   touchValue = touchRead(touchPin);
-
   Serial.print("Touch Value: ");
   Serial.println(touchValue);
-
-  delay(100);
+  do {
+    touchValue = touchRead(touchPin);
+    delay(500);
+  } while (touchValue < TOUCHTHRESHOLD);
+  Serial.println("*** Touch Detected ***");
+  Serial.print("Touch Value: ");
+  Serial.println(touchValue);
 }
