c50d52f84de491c1bc048f2c0d09a86bad32b482
ooikk <105415945+ooikk@users.noreply.github.com>
2026-06-06 23:03:31 +0800

    Add files via upload

---
+++ 06_3.5_TFT_Touch_Display/06_3.5_TFT_Touch_Display.ino
@@ -0,0 +1,14 @@
+#include <TFT_eSPI.h>
+#include <SPI.h>
+
+TFT_eSPI tft = TFT_eSPI();
+
+void setup() {
+  Serial.begin(115200);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
