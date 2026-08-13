694ba2d4f79c897add992cd200c7011544ff10e6
ooikk <105415945+ooikk@users.noreply.github.com>
2026-08-14 00:16:08 +0800

    Add files via upload

--- 02_MISC/02_WiFi/02_MQTT.ino
+++ 02_MISC/02_WiFi/02_MQTT.ino
@@ -2,10 +2,17 @@
 #include <PubSubClient.h>
 #include <WiFiClientSecure.h>
 
+//**************************************************
 // Select one
 //#define HIVEMQ
-#define EMQX
-#define EMQX_CLOUD   // must also enable EMQX
+//#define EMQX
+//#define EMQX_CLOUD   // connect to Googlesheet
+#define ADAFRUIT
+//**************************************************
+
+#if defined(EMQX) || defined(EMQX_CLOUD) || defined(ADAFRUIT)
+#define SECURE_LOGIN
+#endif
 
 // ----------------------------
 // User configuration
@@ -17,41 +24,74 @@ const char* WIFI_PASSWORD = "1122334455";
 // Public Broker Configuration
 // ----------------------------
 #ifdef HIVEMQ
-const char* MQTT_HOST     = "broker.hivemq.com"; // Free public broker
-const uint16_t MQTT_PORT  = 1883;                // Standard TCP port for ESP32
-const char* MQTT_USERNAME = "";                  // No username needed
-const char* MQTT_PASSWORD = "";                  // No password needed
+const char* MQTT_HOST = "broker.hivemq.com";  // Free public broker
+const uint16_t MQTT_PORT = 1883;              // Standard TCP port for ESP32
+#endif
+
+#ifdef EMQX_CLOUD
+const char* MQTT_HOST = "ffcebc18.ala.asia-southeast1.emqxsl.com";  // Fee public broker
+const uint16_t MQTT_PORT = 8883;                                    // Standard TCP port for ESP32
+#endif
+
+#ifdef EMQX
+const char* MQTT_HOST = "broker.emqx.io";  //"ffcebc18.ala.asia-southeast1.emqxsl.com"; // Free public broker
+const uint16_t MQTT_PORT = 8883;           // Standard TCP port for ESP32
+#endif
+
+#ifdef ADAFRUIT
+const char* MQTT_HOST = "io.adafruit.com";  //
+const uint16_t MQTT_PORT = 8883;            // Standard TCP port for ESP32
+#endif
+
+#ifdef SECURE_LOGIN
+#ifdef ADAFRUIT
+const char* MQTT_USERNAME = "ooikk";                             // YOUR_ADAFRUIT_USERNAME
+const char* MQTT_PASSWORD = "aio_ySnz63FVKOkm6Np4bbHiiFy5ZWPJ";  // YOUR_ADAFRUIT_IO_KEY
+#else
+const char* MQTT_USERNAME = "esp32s3";      // username
+const char* MQTT_PASSWORD = "esp11223344";  // password
+#endif
+// ----------------------------
+// Global objects
+// ----------------------------
+WiFiClientSecure secureClient;
+PubSubClient mqtt(secureClient);
+
+#else
+
+const char* MQTT_USERNAME = "";  // No username needed
+const char* MQTT_PASSWORD = "";  // No password needed
 // ----------------------------
 // Global objects
 // ----------------------------
 WiFiClient tcpClient;
 PubSubClient mqtt(tcpClient);
 
-#endif 
-#ifdef EMQX
-  #ifdef EMQX_CLOUD
-   const char* MQTT_HOST = "ffcebc18.ala.asia-southeast1.emqxsl.com"; // Free public broker
-  #else
-   const char* MQTT_HOST = "broker.emqx.io";   //"ffcebc18.ala.asia-southeast1.emqxsl.com"; // Free public broker
-  #endif
-const uint16_t MQTT_PORT = 8883;            // Standard TCP port for ESP32
-const char* MQTT_USERNAME = "esp32s3";      // No username needed
-const char* MQTT_PASSWORD = "esp11223344";  // No password needed
-// ----------------------------
-// Global objects
-// ----------------------------
-WiFiClientSecure secureClient;
-PubSubClient mqtt(secureClient);
 #endif
+
 
 // ----------------------------
 // MQTT topics
 // ----------------------------
+#ifdef ADAFRUIT
+
+//const char* TOPIC_TELEMETRY = "esp32s3/telemetry";
+//const char* TOPIC_LED_SET = "esp32s3/led/set";
+//const char* TOPIC_LED_STATE = "esp32s3/led/state";
+
+const char* TOPIC_STATUS = "ooikk/feeds/status";
+const char* TOPIC_TEMP = "ooikk/feeds/temperature";
+const char* TOPIC_RSSI = "ooikk/feeds/rssi";
+const char* TOPIC_UPTIME = "ooikk/feeds/uptime";
+const char* TOPIC_LED_SET = "ooikk/feeds/led-control";
+const char* TOPIC_LED_STATE = "ooikk/feeds/led-state";  // Adafruit IO feeds are bidirectional:
+
+#else
 const char* TOPIC_STATUS = "esp32s3/status";
 const char* TOPIC_TELEMETRY = "esp32s3/telemetry";
 const char* TOPIC_LED_SET = "esp32s3/led/set";
 const char* TOPIC_LED_STATE = "esp32s3/led/state";
-
+#endif
 
 // ----------------------------
 // Hardware configuration
@@ -68,7 +108,7 @@ const uint32_t MQTT_RECONNECT_INTERVAL_MS = 2000;
 
 
 
-bool ledState = false;
+bool ledState = true;
 uint32_t lastTelemetryMs = 0;
 uint32_t lastMqttConnectAttemptMs = 0;
 
@@ -130,12 +170,20 @@ void mqttCallback(char* topic, byte* payload, unsigned int length) {
     if (turnOn) {
       ledState = true;
       digitalWrite(LED_PIN, HIGH);
+#ifdef ADAFRUIT
       mqtt.publish(TOPIC_LED_STATE, "1", true);
+#else
+      mqtt.publish(TOPIC_LED_STATE, "{\"state\":\"1\"}");
+#endif
       Serial.println("LED turned ON");
     } else if (turnOff) {
       ledState = false;
       digitalWrite(LED_PIN, LOW);
+#ifdef ADAFRUIT
       mqtt.publish(TOPIC_LED_STATE, "0", true);
+#else
+      mqtt.publish(TOPIC_LED_STATE, "{\"state\":\"0\"}");
+#endif
       Serial.println("LED turned OFF");
     } else {
       Serial.println("Unknown LED command");
@@ -162,7 +210,7 @@ void connectMQTT() {
     return;
   }
 
-// Build a completely unique Client ID with random suffix
+  // Build a completely unique Client ID with random suffix
   String clientId = "esp32s3-";
   clientId += WiFi.macAddress();
   clientId.replace(":", "");
@@ -175,10 +223,10 @@ void connectMQTT() {
   // If the device disconnects unexpectedly, broker publishes "offline".
   bool connected = mqtt.connect(
     clientId.c_str(),
-#ifdef EMQX    
+#ifdef SECURE_LOGIN
     MQTT_USERNAME,
     MQTT_PASSWORD,
-#endif    
+#endif
     TOPIC_STATUS,
     0,         // Will QoS
     true,      // Will retain
@@ -195,7 +243,12 @@ void connectMQTT() {
     mqtt.subscribe(TOPIC_LED_SET, 1);
 
     // Report current LED state.
+#ifdef ADAFRUIT
     mqtt.publish(TOPIC_LED_STATE, ledState ? "1" : "0", true);
+#else
+    mqtt.publish(TOPIC_LED_STATE, ledState ? "{\"state\":\"1\"}" : "{\"state\":\"0\"}", true);
+#endif
+
   } else {
     Serial.printf("MQTT connection failed, state code: %d\n", mqtt.state());
   }
@@ -210,8 +263,24 @@ void publishTelemetry() {
   }
 
   // Replace this with a real sensor reading.
-  int temperature = random(20, 30);
+  int temperature = random(10, 40);
+#ifdef ADAFRUIT
+  // Publishing individual feeds (Adafruit IO prefers individual feed updates)
+  mqtt.publish(TOPIC_STATUS, "online", true);  // true: retained
+  delay(2000);                                 // prevent THROTTLE WARNING
+  mqtt.publish(TOPIC_TEMP, String(temperature).c_str());
+  delay(2000);
+  mqtt.publish(TOPIC_RSSI, String((long)WiFi.RSSI()).c_str());
+  delay(2000);
+  mqtt.publish(TOPIC_UPTIME, String((unsigned long long)(millis() / 1000)).c_str());
+  delay(2000);
+  Serial.printf("Published Telemetry** Temp: %d C RSSI: %ld Uptime: %llu LED State: %s\n",
+                temperature,
+                (long)WiFi.RSSI(),
+                ((unsigned long long)(millis() / 1000)),
+                ledState ? "1" : "0");
 
+#else
   char payload[128];
   snprintf(
     payload,
@@ -228,6 +297,7 @@ void publishTelemetry() {
   } else {
     Serial.println("Telemetry publish failed");
   }
+#endif
 }
 
 // ----------------------------
@@ -246,13 +316,13 @@ void setup() {
 
   connectWiFi();
 
-#ifdef EMQX
-// --- Quick test mode: skip certificate verification ---
+#ifdef SECURE_LOGIN
+  // --- Quick test mode: skip certificate verification ---
   secureClient.setInsecure();
 
-// Increase SSL handshake timeout to handle cloud latency (in seconds)
+  // Increase SSL handshake timeout to handle cloud latency (in seconds)
   secureClient.setHandshakeTimeout(30);
-#endif 
+#endif
 
   mqtt.setServer(MQTT_HOST, MQTT_PORT);
   mqtt.setCallback(mqttCallback);
